1ecc0d476893bcb63f2fbe6e18b10d9a4b197603
iMRX Tech <imrxtech@iMRXs-MacBook-Pro.local>
2025-08-30 14:50:45 +0700

    Update

--- app.py
+++ app.py
@@ -76,4 +76,4 @@ if __name__ == '__main__':
         os.makedirs("uploads")
 
     # Run the Flask app
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
